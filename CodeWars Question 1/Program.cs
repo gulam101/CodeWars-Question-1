e0076620812b0bad81b05cbe72773920950fc0ab
unknown <gfram@hotmail.com>
2019-10-14 20:48:04 +0100

    first commit

---
+++ CodeWars Question 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars_Question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class CustomMath
+    {
+        public static int multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
